29541b04c4681f6d574ccc74c5ffc8b021170b38
BoureanuCosmin <lioncoszmin234@gmail.com>
2024-04-17 21:16:41 +0300

    Second commit

---
+++ Project4_DataType/data_type.py
@@ -0,0 +1,78 @@
+#### String ####
+
+# literal assignment
+first = "Cosmin"
+last = "Boureanu"
+
+# print("")
+# print(type(first))
+# print(first)
+# print("")
+
+#### Constructor Function ####
+
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+# print("")
+
+#### Concatenation ####
+
+# fullname = first + " " + last
+# print(fullname)
+
+# fullname += "!"
+# print(fullname)
+
+#### Cast a nr to str ####
+
+# decade = str(1980)
+# print(type(decade))
+# print(decade)
+
+# statemenet = "I like jazz music from the " + decade + "s."
+# print(statemenet)
+
+
+#### Multiple Lines ####
+
+# multiline = """
+# Salut, ce faci?
+
+# Doar verificam.
+#                     Totul bine?
+
+# """
+# print(multiline)
+
+# # Special characters
+
+# special = "I'm back at work!\tHey!\n\nWhere's this at\\located?"
+# print(special)
+
+# #### String Methods ####
+
+# print(first)
+# print(first.lower())
+# print(first.upper())
+# print(first)
+
+# print(multiline.title())
+# print(multiline.replace("bine", "ok"))
+# print(multiline)
+
+# print(len(multiline))
+# multiline += "                                         "
+# multiline += "                             " + multiline
+# print(len(multiline))
+# print(len(multiline.strip()))
+# print(len(multiline.lstrip()))
+# print(len(multiline.rstrip()))
+
+# print("")
+
+# # Build a menu
+
+title = "menu".upper()  #!!!!! Folositor !!!!!
+print(title.center(20, "="))  #!!!!! Folositor !!!!!
